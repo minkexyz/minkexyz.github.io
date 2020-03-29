46f4c2441773b803ab02365882d2270a06abc7f3
minkexyz <59121730+minkexyz@users.noreply.github.com>
2020-03-29 17:53:28 -0400

    Clone from Samsrecipes.
    Copied code over from samrecipes to start storing some recipes online. To do:
     - Update styles
     - Add more functionality

---
+++ main.js
@@ -0,0 +1,129 @@
+
+
+jQuery(document).ready(function() {
+
+	// read JSON file for list of recipes
+    $.getJSON('recipes.json', function(data) {
+
+    	var recipeList = [];
+
+    	// loop through list of recipes
+    	$.each(data.recipes, function(key, recipe) {
+    		recipeList.push('<div class="recipe-item col-md-4 col-sm-6 ');
+    		for (var i in recipe.categories) {
+    			recipeList.push(recipe.categories[i] + ' ');
+    		}
+    		recipeList.push('"><div class="panel panel-default"><div class="panel-heading">' + recipe.title + '</div><div class="panel-body"><p><img class="card-icon" src="img/users.svg">serves: <strong>' + recipe.serves + '</strong></p><p><img class="card-icon" src="img/clock.svg">prep: <strong>' + recipe.prep + '</strong></p><p><img class="card-icon" src="img/clock.svg">wait: <strong>' + recipe.wait + '</strong></p><button type="button" class="btn btn-default" data-toggle="modal" data-designation="' + recipe.designation + '" data-target="#recipe-modal">View Recipe</button></div></div></div>');
+    	});
+
+    	// add all recipes to main section
+    	$('.recipe-block').html(recipeList.join(''));
+
+    });
+
+    $('#search-box').keyup(function() {
+    	var matcher = new RegExp($(this).val(), 'gi');
+    	$('.recipe-item').show().not(function() {
+    		return matcher.test($(this).find('.panel-heading').text());
+    	}).hide();
+    });
+
+    $('#recipe-modal').on('show.bs.modal', function(e) {
+
+    	var button = $(e.relatedTarget);
+    	var details = button.data('designation');
+
+    	var recipeTitle = "";
+    	var recipeIngredients = [];
+    	var recipeSteps = [];
+
+    	$.getJSON('recipes.json', function(data) {
+    		recipeTitle = data.recipes[details].title;
+    		for (var j in data.recipes[details].ingredients) {
+    			recipeIngredients.push('<li>' + data.recipes[details].ingredients[j] + '</li>');
+    		};
+    		for (var k in data.recipes[details].instructions) {
+    			recipeSteps.push('<li>' + data.recipes[details].instructions[k] + '</li>');
+    		};
+
+    		$('.modal-title').html(recipeTitle);
+    		$('.modal-ingredients-list').html(recipeIngredients);
+    		$('.modal-steps-list').html(recipeSteps);
+
+    	});
+
+    });
+
+    // Sidebar filtering
+    $('#appetizer-nav').click(function(e) {
+		e.preventDefault;
+		hideAllRecipes();
+		$('.appetizer').show();
+	});
+
+	$('#bread-nav').click(function(e) {
+		e.preventDefault;
+		hideAllRecipes();
+		$('.bread').show();
+	});
+
+	$('#breakfast-nav').click(function(e) {
+		e.preventDefault;
+		hideAllRecipes();
+		$('.breakfast').show();
+	});
+
+	$('#dessert-nav').click(function(e) {
+		e.preventDefault;
+		hideAllRecipes();
+		$('.dessert').show();
+	});
+
+	$('#dinner-nav').click(function(e) {
+		e.preventDefault;
+		hideAllRecipes();
+		$('.dinner').show();
+	});
+
+	$('#smoothie-nav').click(function(e) {
+		e.preventDefault;
+		hideAllRecipes();
+		$('.smoothie').show();
+	});
+
+	$('#snack-nav').click(function(e) {
+		e.preventDefault;
+		hideAllRecipes();
+		$('.snack').show();
+	});
+
+	$('#soup-nav').click(function(e) {
+		e.preventDefault;
+		hideAllRecipes();
+		$('.soup').show();
+	});
+
+	$('#all-nav').click(function(e) {
+		e.preventDefault;
+		$('.recipe-item').show();
+	});
+
+	$('.nav-sidebar li').click(function(e) {
+		e.preventDefault();
+		removeActive();
+		$(this).addClass('active');
+	});
+
+});
+
+function removeActive() {
+	$('.nav-sidebar li').removeClass('active');
+}
+
+function hideAllRecipes() {
+	$('.recipe-item').hide();
+}
+
+function randomRecipe() {
+	return Math.floor(Math.random() * 12);
+}
